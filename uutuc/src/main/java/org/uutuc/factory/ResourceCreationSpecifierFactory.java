d90347bec264a754babebb897da4475b566a8167
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2009-02-20 00:37:09 +0000

    initial import from mime repository.

---
+++ uutuc/src/main/java/org/uutuc/factory/ResourceCreationSpecifierFactory.java
@@ -0,0 +1,161 @@
+/* 
+ Copyright 2009 Regents of the University of Colorado.  
+ All rights reserved. 
+
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at 
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+
+ Unless required by applicable law or agreed to in writing, software 
+ distributed under the License is distributed on an "AS IS" BASIS, 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+*/
+
+package org.uutuc.factory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.resource.ResourceCreationSpecifier;
+import org.apache.uima.resource.metadata.ConfigurationParameter;
+import org.apache.uima.resource.metadata.ConfigurationParameterDeclarations;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+import org.apache.uima.resource.metadata.ResourceMetaData;
+import org.apache.uima.resource.metadata.impl.ConfigurationParameter_impl;
+import org.apache.uima.util.XMLInputSource;
+import org.apache.uima.util.XMLParser;
+/**
+ * @author Steven Bethard, Philip Ogren
+ */
+public class ResourceCreationSpecifierFactory {
+
+	
+	/**
+	 * Parse a ResourceCreationSpecifier from the URL of an XML descriptor file,
+	 * setting additional configuration parameters as necessary.
+	 * 
+	 * @param descriptorURL
+	 *            The URL of the XML descriptor file.
+	 * @param parameters
+	 *            Any additional configuration parameters to be set. These
+	 *            should be supplied as (name, value) pairs, so there should
+	 *            always be an even number of parameters.
+	 * @return The ResourceCreationSpecifier for the XML descriptor with all the
+	 *         configuration parameters set.
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
+	public static ResourceCreationSpecifier createResourceCreationSpecifier(URL descriptorURL, Object[] parameters)
+	throws UIMAException, IOException {
+		return createResourceCreationSpecifier(new XMLInputSource(descriptorURL), parameters);
+	}
+
+	/**
+	 * Parse a ResourceCreationSpecifier from XML descriptor file input,
+	 * setting additional configuration parameters as necessary.
+	 * 
+	 * @param xmlInput
+	 *            The descriptor file as an XMLInputSource.
+	 * @param parameters
+	 *            Any additional configuration parameters to be set. These
+	 *            should be supplied as (name, value) pairs, so there should
+	 *            always be an even number of parameters.
+	 * @return The ResourceCreationSpecifier for the XML descriptor with all the
+	 *         configuration parameters set.
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
+	public static ResourceCreationSpecifier createResourceCreationSpecifier(XMLInputSource xmlInput, Object[] parameters)
+	throws UIMAException, IOException {
+		ResourceCreationSpecifier specifier;
+		XMLParser parser = UIMAFramework.getXMLParser();
+		specifier = (ResourceCreationSpecifier) parser.parseResourceSpecifier(xmlInput);
+		setConfigurationParameters(specifier, parameters);
+		return specifier;
+	}
+
+	/**
+	 * Parse a ResourceCreationSpecifier from an XML descriptor file, setting
+	 * additional configuration parameters as necessary.
+	 * 
+	 * @param descriptorPath
+	 *            The path to the XML descriptor file.
+	 * @param parameters
+	 *            Any additional configuration parameters to be set. These
+	 *            should be supplied as (name, value) pairs, so there should
+	 *            always be an even number of parameters.
+	 * @return The ResourceCreationSpecifier for the XML descriptor with all the
+	 *         configuration parameters set.
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
+	public static ResourceCreationSpecifier createResourceCreationSpecifier(String descriptorPath, Object[] parameters)
+	throws UIMAException, IOException {
+		return createResourceCreationSpecifier(new XMLInputSource(descriptorPath), parameters);
+	}
+
+	/**
+	 * Create configuration parameter declarations and settings from a list of
+	 * (name, value) pairs.
+	 * 
+	 * @param specifier
+	 *            The ResourceCreationSpecifier whose parameters are to be set.
+	 * @param configurationParameters
+	 *            The configuration parameters to be set. These should be
+	 *            supplied as (name, value) pairs, so there should always be an
+	 *            even number of parameters.
+	 */
+	public static void setConfigurationParameters(ResourceCreationSpecifier specifier,
+			Object... configurationParameters) {
+		if (configurationParameters.length % 2 != 0) {
+			String message = "a value must be specified for each parameter name: an odd number of values passed in ("+configurationParameters.length+")";
+			throw new IllegalArgumentException(message);
+		}
+		ResourceMetaData metaData = specifier.getMetaData();
+		ConfigurationParameterDeclarations paramDecls = metaData.getConfigurationParameterDeclarations();
+		ConfigurationParameterSettings paramSettings = metaData.getConfigurationParameterSettings();
+		for (int i = 0; i < configurationParameters.length; i += 2) {
+			String name = (String) configurationParameters[i];
+			Object value = configurationParameters[i + 1];
+
+			Class<?> javaClass = value.getClass();
+			String javaClassName;
+			if (javaClass.isArray()) javaClassName = javaClass.getComponentType().getName();
+			else javaClassName = javaClass.getName();
+
+			String uimaType = javaUimaTypeMap.get(javaClassName);
+			if (uimaType == null) {
+				String message = "invalid parameter type " + javaClassName;
+				throw new IllegalArgumentException(message);
+			}
+			ConfigurationParameter param = new ConfigurationParameter_impl();
+			param.setName(name);
+			param.setType(uimaType);
+			param.setMultiValued(javaClass.isArray());
+			paramDecls.addConfigurationParameter(param);
+			paramSettings.setParameterValue(name, value);
+		}
+
+	}
+
+	/**
+	 * A mapping from Java class names to UIMA configuration parameter type
+	 * names. Used by setConfigurationParameters().
+	 */
+	public static final Map<String, String> javaUimaTypeMap = new HashMap<String, String>();
+	static {
+		javaUimaTypeMap.put(Boolean.class.getName(), ConfigurationParameter.TYPE_BOOLEAN);
+		javaUimaTypeMap.put(Float.class.getName(), ConfigurationParameter.TYPE_FLOAT);
+		javaUimaTypeMap.put(Integer.class.getName(), ConfigurationParameter.TYPE_INTEGER);
+		javaUimaTypeMap.put(String.class.getName(), ConfigurationParameter.TYPE_STRING);
+	};
+
+}
---
+++ uutuc/src/main/java/org/uutuc/util/Tokenizer.java
@@ -0,0 +1,117 @@
+/* 
+ Copyright 2009 Regents of the University of Colorado.  
+ All rights reserved. 
+
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at 
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+
+ Unless required by applicable law or agreed to in writing, software 
+ distributed under the License is distributed on an "AS IS" BASIS, 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+*/
+
+package org.uutuc.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.uutuc.factory.AnnotationFactory;
+
+/**
+ * This class was written as a simple way to populate a JCas with tokens along
+ * with part-of-speech and stem information and assumes you already have this
+ * information at your disposal. This should not be confused with an actual
+ * tokenizer (or part-of-speech tagger) which tokenizes and tags plain text.  
+ * 
+ * @author Steven Bethard, Philip Ogren
+ */
+public class Tokenizer {
+
+	public static <TOKEN_TYPE extends Annotation, SENTENCE_TYPE extends Annotation> void createTokens(JCas jCas,
+			String text, Class<TOKEN_TYPE> tokenClass, Class<SENTENCE_TYPE> sentenceClass) throws UIMAException {
+		createTokens(jCas, text, tokenClass, sentenceClass, null, null, null, null, null);
+	}
+
+	public static <TOKEN_TYPE extends Annotation, SENTENCE_TYPE extends Annotation> void createTokens(JCas jCas,
+			String text, Class<TOKEN_TYPE> tokenClass, Class<SENTENCE_TYPE> sentenceClass, String tokensString)
+			throws UIMAException {
+		createTokens(jCas, text, tokenClass, sentenceClass, tokensString, null, null, null, null);
+	}
+
+	/**
+	 * Add Token and Sentence annotations to the JCas.
+	 * 
+	 * @param jCas
+	 *            The JCas where the annotation should be added.
+	 * @param text
+	 *            The text of the document.
+	 * @param tokensString
+	 *            The text of the document with spaces separating words and
+	 *            newlines separating sentences.
+	 * @param posTagsString
+	 *            The part of speech tags for each word, separated by spaces.
+	 * @param stemsString
+	 *            The stems for each word, separated by spaces.
+	 * @throws UIMAException
+	 */
+	public static <TOKEN_TYPE extends Annotation, SENTENCE_TYPE extends Annotation> void createTokens(JCas jCas,
+			String text, Class<TOKEN_TYPE> tokenClass, Class<SENTENCE_TYPE> sentenceClass, String tokensString,
+			String posTagsString, String stemsString, String posFeatureName, String stemFeatureName)
+			throws UIMAException {
+		// set the document text and add Token annotations as indicated
+		jCas.setDocumentText(text);
+		int offset = 0;
+		int tokenIndex = 0;
+
+		Feature posFeature = null;
+		if (posFeatureName != null) posFeature = jCas.getTypeSystem().getFeatureByFullName(posFeatureName);
+		Feature stemFeature = null;
+		if (stemFeatureName != null) stemFeature = jCas.getTypeSystem().getFeatureByFullName(stemFeatureName);
+
+		String[] sentenceStrings = (tokensString != null ? tokensString : text).split("\n");
+		String[] posTags = posTagsString != null ? posTagsString.split(" ") : null;
+		String[] stems = stemsString != null ? stemsString.split(" ") : null;
+		for (String sentenceString : sentenceStrings) {
+			String[] tokenStrings = sentenceString.trim().split(" ");
+			List<Annotation> tokenAnnotations = new ArrayList<Annotation>();
+			for (String tokenString : tokenStrings) {
+				// move the offset up to the beginning of the token
+				while (!text.startsWith(tokenString, offset)) {
+					offset++;
+					if (offset > text.length()) {
+						throw new IllegalArgumentException(String.format("unable to find string %s", tokenString));
+					}
+				}
+
+				// add the Token
+				int start = offset;
+				offset = offset + tokenString.length();
+				Annotation token = AnnotationFactory.createAnnotation(jCas, start, offset, tokenClass);
+				tokenAnnotations.add(token);
+
+				// set the stem and part of speech if present
+				if (posFeatureName != null) {
+					token.setStringValue(posFeature, posTags[tokenIndex]);
+				}
+				if (stemFeatureName != null) {
+					token.setStringValue(stemFeature, stems[tokenIndex]);
+				}
+				tokenIndex++;
+			}
+			if (tokenAnnotations.size() > 0) {
+				int begin = tokenAnnotations.get(0).getBegin();
+				int end = tokenAnnotations.get(tokenAnnotations.size() - 1).getEnd();
+				AnnotationFactory.createAnnotation(jCas, begin, end, sentenceClass);
+			}
+		}
+	}
+}
