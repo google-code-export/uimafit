7f2d73805f55081e92be794c805b462d9ad791c0
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2009-07-20 22:51:00 +0000

    corresponds to version 0.9.5 release

---
+++ tags/0.9.5/src/test/java/org/uutuc/type/Sentence.java
@@ -0,0 +1,61 @@
+
+
+/* First created by JCasGen Thu Feb 05 20:53:17 MST 2009 */
+package org.uutuc.type;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Thu Feb 05 20:53:52 MST 2009
+ * XML source: C:/Documents and Settings/Philip/My Documents/CSLR/workspace/uutuc/test/desc/TestTypeSystem.xml
+ * @generated */
+public class Sentence extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  public final static int typeIndexID = JCasRegistry.register(Sentence.class);
+  /** @generated
+   * @ordered 
+   */
+  public final static int type = typeIndexID;
+  /** @generated  */
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Sentence() {}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public Sentence(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public Sentence(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public Sentence(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {}
+     
+}
+
+    
