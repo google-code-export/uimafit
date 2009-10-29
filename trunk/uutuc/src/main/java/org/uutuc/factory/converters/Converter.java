ab51887d8692debfa286d6b767d2de5893cd3b5e
richard.eckart <richard.eckart@4103b874-978e-a562-9e03-8f04a24f2d1c>
2009-10-29 12:59:48 +0000

    Example implementation using converters. I have this integrated into my own copy of uutuc and for me the test cases are all green. I tried porting my testcase for collections to the official uutuc, but it is choking on collections in the configurationparameterfactory.

---
+++ trunk/uutuc/src/main/java/org/uutuc/factory/converters/Converter.java
@@ -0,0 +1,33 @@
+/*
+ Copyright 2009
+ Ubiquitous Knowledge Processing (UKP) Lab
+ Technische Universitaet Darmstadt
+ All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package org.uutuc.factory.converters;
+
+/**
+ * Interface for converters that convert default values to actual field values.
+ * 
+ * @author Richard Eckart de Castilho
+ */
+public interface Converter
+{
+//	Cardinality getRequiredCardinality();
+	Class<?> getUimaType();
+	Class<?> getResultType();
+	Object convert(Object value);
+}
---
+++ trunk/uutuc/src/main/java/org/uutuc/factory/converters/NullConverter.java
@@ -0,0 +1,46 @@
+/*
+ Copyright 2009
+ Ubiquitous Knowledge Processing (UKP) Lab
+ Technische Universitaet Darmstadt
+ All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package org.uutuc.factory.converters;
+
+/**
+ * Null converter that is used to indicate that default converters should be 
+ * used.
+ * 
+ * @author Richard Eckart de Castilho
+ */
+public class NullConverter implements Converter
+{
+	public Object convert(Object aValue)
+	{
+		return null;
+	}
+	
+	public Class<?> getResultType()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Class<?> getUimaType()
+	{
+		return null;
+	}
+
+}
