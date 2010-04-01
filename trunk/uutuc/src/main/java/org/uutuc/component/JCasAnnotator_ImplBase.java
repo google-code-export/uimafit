ce9b6e86fcbe799afb81f75cb96b9c21c0d42e9a
richard.eckart <richard.eckart@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-04-01 08:08:08 +0000

    Added component base classes. Added "provided" dependency on findbugs annotations. These are not retained at runtime, so using them does not introduct another runtime dependency.

---
+++ trunk/uutuc/src/main/java/org/uutuc/component/JCasAnnotator_ImplBase.java
@@ -0,0 +1,22 @@
+package org.uutuc.component;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uutuc.factory.ExternalResourceConfigurator;
+import org.uutuc.util.InitializeUtil;
+
+import edu.umd.cs.findbugs.annotations.OverrideMustInvoke;
+
+public abstract class JCasAnnotator_ImplBase
+	extends org.apache.uima.analysis_component.JCasAnnotator_ImplBase
+{
+	@Override
+	@OverrideMustInvoke
+	public void initialize(UimaContext aContext)
+		throws ResourceInitializationException
+	{
+		super.initialize(aContext);
+		InitializeUtil.initialize(this, aContext);
+		ExternalResourceConfigurator.configure(aContext, this);
+	}
+}
