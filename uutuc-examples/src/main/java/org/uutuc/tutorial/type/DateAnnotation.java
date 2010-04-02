25a50e63a4cd828ad4216149ecf72da20051efac
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-04-02 19:48:36 +0000

    adding type folder

---
+++ uutuc-examples/src/main/java/org/uutuc/tutorial/type/DateAnnotation.java
@@ -0,0 +1,61 @@
+
+
+/* First created by JCasGen Fri Apr 02 09:48:30 MDT 2010 */
+package org.uutuc.tutorial.type;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+
+
+/** 
+ * Updated by JCasGen Fri Apr 02 11:08:51 MDT 2010
+ * XML source: C:/Users/Philip/Documents/Academic/workspace/uutuc-examples/src/main/resources/org/uutuc/tutorial/type/TypeSystem.xml
+ * @generated */
+public class DateAnnotation extends DateTimeAnnotation {
+  /** @generated
+   * @ordered 
+   */
+  public final static int typeIndexID = JCasRegistry.register(DateAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  public final static int type = typeIndexID;
+  /** @generated  */
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected DateAnnotation() {}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public DateAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public DateAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public DateAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {}
+     
+}
+
+    
