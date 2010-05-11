30ee798fef906409ba8da3354b15d441caacd935
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-05-11 17:08:05 +0000

    reverting changes from last commit.

--- trunk/uimaFIT/src/main/java/org/uimafit/factory/AggregateBuilder.java
+++ trunk/uimaFIT/src/main/java/org/uimafit/factory/AggregateBuilder.java
@@ -128,8 +128,10 @@ public class AggregateBuilder {
 			sofaMappings.add(SofaMappingFactory.createSofaMapping(componentName, componentViewName, aggregateViewName));
 		}
 		else {
-			throw new IllegalArgumentException("No component with the name '" + componentName
-					+ "' has been added to this builder.  Sofa mappings may only be added for components that have been added to this builder. ");
+			throw new IllegalArgumentException(
+					"No component with the name '"
+							+ componentName
+							+ "' has been added to this builder.  Sofa mappings may only be added for components that have been added to this builder. ");
 		}
 	}
 
@@ -142,8 +144,8 @@ public class AggregateBuilder {
 	 * @throws ResourceInitializationException
 	 */
 	public AnalysisEngine createAggregate() throws ResourceInitializationException {
-		return AnalysisEngineFactory.createAggregate(analysisEngineDescriptions, componentNames, typeSystemDescription, typePriorities, sofaMappings
-				.toArray(new SofaMapping[sofaMappings.size()]));
+		return AnalysisEngineFactory.createAggregate(analysisEngineDescriptions, componentNames, typeSystemDescription,
+				typePriorities, sofaMappings.toArray(new SofaMapping[sofaMappings.size()]));
 	}
 
 	/**
@@ -155,8 +157,8 @@ public class AggregateBuilder {
 	 * @throws ResourceInitializationException
 	 */
 	public AnalysisEngineDescription createAggregateDescription() throws ResourceInitializationException {
-		return AnalysisEngineFactory.createAggregateDescription(analysisEngineDescriptions, componentNames, typeSystemDescription, typePriorities,
-				sofaMappings.toArray(new SofaMapping[sofaMappings.size()]));
+		return AnalysisEngineFactory.createAggregateDescription(analysisEngineDescriptions, componentNames,
+				typeSystemDescription, typePriorities, sofaMappings.toArray(new SofaMapping[sofaMappings.size()]));
 	}
 
 }
