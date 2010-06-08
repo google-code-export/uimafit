ae2e5ade54f61823cc279feb930946bbef920ffc
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-06-08 21:17:34 +0000

    changed super class to the uimafit component.  updated javadoc description of the class

--- trunk/uimaFIT-examples/src/main/java/org/uimafit/tutorial/ex1/RoomNumberAnnotator.java
+++ trunk/uimaFIT-examples/src/main/java/org/uimafit/tutorial/ex1/RoomNumberAnnotator.java
@@ -24,18 +24,29 @@ import java.io.FileOutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.uimafit.component.JCasAnnotator_ImplBase;
 import org.uimafit.descriptor.TypeCapability;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
 import org.uimafit.tutorial.type.RoomNumber;
 
 /**
- * Example annotator that detects room numbers using Java 1.4 regular
- * expressions.
+ * This class was copied from the uimaj-examples project and modified in following ways:
+ * <ul><li>The package name was changed to org.uimafit.tutorial.ex1</li>
+ * 	<li>The super class was changed to org.uimafit.component.JCasAnnotator_ImplBase</li>
+ * 	<li>The class is annotated with org.uimafit.descriptor.TypeCapability</li>
+ *  	<li>a main method that generates a descriptor file was added.</li>
+ *  </ul>
+ *  
+ *  This class has two key differences related to uimaFIT.  First, the type capabilities are annotated using the @TypeCapability annotation which are used by the 
+ *  AnalysisEngineFactory to specify this information in the descriptors returned by, e.g. AnalysisEngineFactory.createPrimitive().  Second, there is a main method
+ *  which shows how to generate an xml descriptor file using this class definition.  The resulting XML descriptor file is the same as the one provided in the 
+ *  uimaj-examples except that instead of building the file in parallel with the class definition, it is now built completely by using the class definition.  
+
+ * 
  */
 @TypeCapability(outputs= {"org.apache.uima.tutorial.RoomNumber", "org.apache.uima.tutorial.RoomNumber:building"})
 public class RoomNumberAnnotator extends JCasAnnotator_ImplBase {
