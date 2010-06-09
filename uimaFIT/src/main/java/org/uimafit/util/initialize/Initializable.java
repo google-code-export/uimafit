6cc5e62aba85c9d3ac3605f91d330aa7651d5346
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-06-09 17:42:49 +0000

    - resolved issue #14 "move XWriter into uimaFIT from ClearTK" 	- resolved issue #15 "move XWriter into uimaFIT from ClearTK"

---
+++ uimaFIT/src/main/java/org/uimafit/util/initialize/Initializable.java
@@ -0,0 +1,36 @@
+/* 
+  Copyright 2010 Regents of the University of Colorado.  
+ All rights reserved. 
+
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at 
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+
+ Unless required by applicable law or agreed to in writing, software 
+ distributed under the License is distributed on an "AS IS" BASIS, 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+ */
+
+package org.uimafit.util.initialize;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.resource.ResourceInitializationException;
+
+
+/**
+ * <br>
+ * Copyright (c) 2009, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * @author Philip Ogren
+ * 
+ */
+
+public interface Initializable {
+
+	public void initialize(UimaContext context) throws ResourceInitializationException;
+
+}
