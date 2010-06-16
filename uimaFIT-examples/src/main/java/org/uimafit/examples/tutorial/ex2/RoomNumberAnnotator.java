407f45c1d2c9607dbe6d191a78df5dec9f65a27e
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-06-16 15:26:17 +0000

    Initial commit of tutorial/ex2/RoomNumberAnnotator.java

---
+++ uimaFIT-examples/src/main/java/org/uimafit/examples/tutorial/ex2/RoomNumberAnnotator.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.uimafit.examples.tutorial.ex2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.Level;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.examples.tutorial.type.RoomNumber;
+
+/**
+ * Example annotator that detects room numbers using Java 1.4 regular expressions.
+ */
+public class RoomNumberAnnotator extends JCasAnnotator_ImplBase {
+  
+	@ConfigurationParameter(name="Patterns")
+	private Pattern[] mPatterns;
+
+  @ConfigurationParameter(name="Locations")
+  private String[] mLocations;
+
+  /**
+   * @see JCasAnnotator_ImplBase#process(JCas)
+   */
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    // get document text
+    String docText = aJCas.getDocumentText();
+
+    // loop over patterns
+    for (int i = 0; i < mPatterns.length; i++) {
+      Matcher matcher = mPatterns[i].matcher(docText);
+      while (matcher.find()) {
+        // found one - create annotation
+        RoomNumber annotation = new RoomNumber(aJCas);
+        annotation.setBegin(matcher.start());
+        annotation.setEnd(matcher.end());
+        annotation.addToIndexes();
+        annotation.setBuilding(mLocations[i]);
+        getContext().getLogger().log(Level.FINEST, "Found: " + annotation);
+      }
+    }
+  }
+
+}
