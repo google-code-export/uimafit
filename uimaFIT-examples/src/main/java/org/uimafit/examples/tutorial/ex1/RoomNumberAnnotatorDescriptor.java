26c62fdde4049023a07bf6844dcfbf0906ef0b26
philip@ogren.info <philip@ogren.info@4103b874-978e-a562-9e03-8f04a24f2d1c>
2010-06-16 17:11:59 +0000

    improved javadoc

--- uimaFIT-examples/src/main/java/org/uimafit/examples/tutorial/ex1/RoomNumberAnnotatorDescriptor.java
+++ uimaFIT-examples/src/main/java/org/uimafit/examples/tutorial/ex1/RoomNumberAnnotatorDescriptor.java
@@ -10,13 +10,23 @@ import org.uimafit.examples.tutorial.type.RoomNumber;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
 
+/**
+ * This class provides a main method which shows how to generate an xml
+ * descriptor file using the RoomNumberAnnotator class definition. The resulting
+ * XML descriptor file is the same as the one provided in the uimaj-examples
+ * except that instead of building the file in parallel with the class
+ * definition, it is now built completely by using the class definition.
+ * 
+ * @author Philip Ogren
+ * 
+ */
 public class RoomNumberAnnotatorDescriptor {
 
 	public static AnalysisEngineDescription createDescriptor() throws ResourceInitializationException {
 		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescription(RoomNumber.class);
 		return AnalysisEngineFactory.createPrimitiveDescription(RoomNumberAnnotator.class, typeSystemDescription);
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		File outputDirectory = new File("src/main/resources/org/uimafit/examples/tutorial/ex1/");
 		outputDirectory.mkdirs();
