0399e64c436a2e6223c45e4f6fba333b73982511
richard.eckart@gmail.com <richard.eckart@gmail.com@4103b874-978e-a562-9e03-8f04a24f2d1c>
2011-04-02 14:06:12 +0000

    - Added LicenseTest. - Added missing license headers and author attributions.

--- uimaFIT-spring/src/main/java/org/uimafit/spring/util/ResourceInitializationUtil.java
+++ uimaFIT-spring/src/main/java/org/uimafit/spring/util/ResourceInitializationUtil.java
@@ -1,3 +1,22 @@
+/*
+ Copyright 2011
+ Ubiquitous Knowledge Processing (UKP) Lab
+ Technische Universitaet Darmstadt
+ All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package org.uimafit.spring.util;
 
 import org.apache.uima.analysis_component.AnalysisComponent;
@@ -8,6 +27,9 @@ import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * @author Richard Eckart de Castilho
+ */
 public class ResourceInitializationUtil {
 	/**
 	 * Initialize an existing object as a Spring bean.
