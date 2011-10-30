62f56c69c0c0fc5487ce0272ef75d51d869fee49
richard.eckart@gmail.com <richard.eckart@gmail.com@4103b874-978e-a562-9e03-8f04a24f2d1c>
2011-10-30 14:47:17 +0000

    - Upgrade calls to deprecated API - Removed compiler warnings

--- uimaFIT-examples/src/main/java/org/uimafit/examples/experiment/pos/BaselineTagger.java
+++ uimaFIT-examples/src/main/java/org/uimafit/examples/experiment/pos/BaselineTagger.java
@@ -17,11 +17,12 @@
 
 package org.uimafit.examples.experiment.pos;
 
+import static org.uimafit.util.JCasUtil.select;
+
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.uimafit.component.JCasAnnotator_ImplBase;
 import org.uimafit.examples.type.Token;
-import org.uimafit.util.JCasUtil;
 
 /**
  * This "baseline" part-of-speech tagger isn't very sophisticated! Notice,
@@ -35,7 +36,7 @@ public class BaselineTagger extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas jCas) throws AnalysisEngineProcessException {
-		for (Token token : JCasUtil.iterate(jCas, Token.class)) {
+		for (Token token : select(jCas, Token.class)) {
 			String word = token.getCoveredText();
 			if (word.equals("a") || word.equals("the")) {
 				token.setPos("DT");
--- uimaFIT-examples/src/main/java/org/uimafit/examples/experiment/pos/SentenceAndTokenCopier.java
+++ uimaFIT-examples/src/main/java/org/uimafit/examples/experiment/pos/SentenceAndTokenCopier.java
@@ -18,6 +18,7 @@ package org.uimafit.examples.experiment.pos;
 
 import static org.uimafit.examples.experiment.pos.ViewNames.VIEW1;
 import static org.uimafit.examples.experiment.pos.ViewNames.VIEW2;
+import static org.uimafit.util.JCasUtil.select;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CASException;
@@ -26,7 +27,6 @@ import org.uimafit.component.JCasAnnotator_ImplBase;
 import org.uimafit.descriptor.SofaCapability;
 import org.uimafit.examples.type.Sentence;
 import org.uimafit.examples.type.Token;
-import org.uimafit.util.JCasUtil;
 
 /**
  * This simple AE copies tokens and sentences from one view to another.
@@ -44,18 +44,16 @@ public class SentenceAndTokenCopier extends JCasAnnotator_ImplBase {
 			JCas view1 = jCas.getView(VIEW1);
 			JCas view2 = jCas.getView(VIEW2);
 
-			for (Token token1 : JCasUtil.iterate(view1, Token.class)) {
+			for (Token token1 : select(view1, Token.class)) {
 				new Token(view2, token1.getBegin(), token1.getEnd()).addToIndexes();
 			}
 
-			for (Sentence sentence1 : JCasUtil.iterate(view1, Sentence.class)) {
+			for (Sentence sentence1 : select(view1, Sentence.class)) {
 				new Sentence(view2, sentence1.getBegin(), sentence1.getEnd()).addToIndexes();
 			}
 		}
 		catch (CASException ce) {
 			throw new AnalysisEngineProcessException(ce);
 		}
-
 	}
-
 }
