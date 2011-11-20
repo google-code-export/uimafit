3ceef73e15c84a8c33734da3c0123e0bf6a6e225
richard.eckart@gmail.com <richard.eckart@gmail.com@4103b874-978e-a562-9e03-8f04a24f2d1c>
2011-11-20 18:06:42 +0000

    No issue - use by-name TSD creation in ex1 and automatic detection in ex2.

--- uimaFIT-examples/src/main/java/org/uimafit/examples/tutorial/ex2/RoomNumberAnnotatorPipeline.java
+++ uimaFIT-examples/src/main/java/org/uimafit/examples/tutorial/ex2/RoomNumberAnnotatorPipeline.java
@@ -17,14 +17,11 @@
 package org.uimafit.examples.tutorial.ex2;
 
 import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
-import static org.uimafit.factory.JCasFactory.createJCas;
-import static org.uimafit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
 import static org.uimafit.util.JCasUtil.select;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.uimafit.examples.tutorial.type.RoomNumber;
 
 /**
@@ -35,16 +32,19 @@ import org.uimafit.examples.tutorial.type.RoomNumber;
 public class RoomNumberAnnotatorPipeline {
 
 	public static void main(String[] args) throws UIMAException {
+		AnalysisEngine analysisEngine = createPrimitive(RoomNumberAnnotator.class, "Patterns",
+				new String[] { "\\b[0-4]\\d-[0-2]\\d\\d\\b", "\\b[G1-4][NS]-[A-Z]\\d\\d\\b" },
+				"Locations", new String[] { "Downtown", "Uptown" });
+
+		JCas jCas = analysisEngine.newJCas();
 		String text = "The meeting was moved from Yorktown 01-144 to Hawthorne 1S-W33.";
-		TypeSystemDescription tsd = createTypeSystemDescription("org.uimafit.examples.type.RoomNumber");
-		JCas jCas = createJCas(tsd);
 		jCas.setDocumentText(text);
-		AnalysisEngine analysisEngine = createPrimitive(RoomNumberAnnotator.class, tsd, "Patterns", new String[] { "\\b[0-4]\\d-[0-2]\\d\\d\\b",
-				"\\b[G1-4][NS]-[A-Z]\\d\\d\\b" }, "Locations", new String[] { "Downtown", "Uptown" });
+
 		analysisEngine.process(jCas);
 
 		for (RoomNumber roomNumber : select(jCas, RoomNumber.class)) {
-			System.out.println(roomNumber.getCoveredText() + "\tbuilding = " + roomNumber.getBuilding());
+			System.out.println(roomNumber.getCoveredText() + "\tbuilding = "
+					+ roomNumber.getBuilding());
 		}
 	}
 }
