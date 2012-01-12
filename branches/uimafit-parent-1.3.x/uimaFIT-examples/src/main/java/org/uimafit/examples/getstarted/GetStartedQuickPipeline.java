ce7f5a237760ba84db346528fb5fd0ae97b8cc2a
richard.eckart@gmail.com <richard.eckart@gmail.com@4103b874-978e-a562-9e03-8f04a24f2d1c>
2012-01-12 16:12:43 +0000

    Branching off trunk for 1.3.0 release.

---
+++ branches/uimafit-parent-1.3.x/uimaFIT-examples/src/main/java/org/uimafit/examples/getstarted/GetStartedQuickPipeline.java
@@ -0,0 +1,51 @@
+/* 
+ Copyright 2009-2010	Regents of the University of Colorado.  
+ All rights reserved. 
+
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at 
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+
+ Unless required by applicable law or agreed to in writing, software 
+ distributed under the License is distributed on an "AS IS" BASIS, 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ See the License for the specific language governing permissions and 
+ limitations under the License.
+ */
+package org.uimafit.examples.getstarted;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.JCasFactory;
+import org.uimafit.factory.TypeSystemDescriptionFactory;
+
+/**
+ * 
+ * @author Philip Ogren
+ * 
+ */
+public class GetStartedQuickPipeline {
+
+	public static void main(String[] args) throws UIMAException {
+		// Normally you would pass in a list of type system locations in the classpath here or
+		// you make use of the automatic type system detection if you do not specify any arguments.
+		// In the latter case, the type systems are resolved using the file
+		// META-INF/org.uimafit/types.txt
+		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
+				.createTypeSystemDescription();
+		// uimaFIT doesn't provide any collection readers - so we will just instantiate a JCas and
+		// run it through our AE
+		JCas jCas = JCasFactory.createJCas(typeSystemDescription);
+		// here we will instantiate the analysis engine using the value "uimaFIT" for the parameter
+		// "stringParamName".
+		AnalysisEngine analysisEngine = AnalysisEngineFactory.createPrimitive(
+				GetStartedQuickAE.class, typeSystemDescription, "stringParamName", "uimaFIT");
+		// run the analysis engine and look for a special greeting in your console.
+		analysisEngine.process(jCas);
+	}
+}
